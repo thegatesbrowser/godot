4be02c8248672ed28515db28920949d722414047
Nordup <nordup.ondr@gmail.com>
2025-09-03 21:37:11 +0700

    monitor parent process with sockets and close when disconnected

--- modules/the_gates/command_sync.h
+++ modules/the_gates/command_sync.h
@@ -1,3 +1,33 @@
+/**************************************************************************/
+/*  command_sync.h                                                        */
+/**************************************************************************/
+/*                         This file is part of:                          */
+/*                             GODOT ENGINE                               */
+/*                        https://godotengine.org                         */
+/**************************************************************************/
+/* Copyright (c) 2014-present Godot Engine contributors (see AUTHORS.md). */
+/* Copyright (c) 2007-2014 Juan Linietsky, Ariel Manzur.                  */
+/*                                                                        */
+/* Permission is hereby granted, free of charge, to any person obtaining  */
+/* a copy of this software and associated documentation files (the        */
+/* "Software"), to deal in the Software without restriction, including    */
+/* without limitation the rights to use, copy, modify, merge, publish,    */
+/* distribute, sublicense, and/or sell copies of the Software, and to     */
+/* permit persons to whom the Software is furnished to do so, subject to  */
+/* the following conditions:                                              */
+/*                                                                        */
+/* The above copyright notice and this permission notice shall be         */
+/* included in all copies or substantial portions of the Software.        */
+/*                                                                        */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,        */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF     */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. */
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY   */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,   */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE      */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 */
+/**************************************************************************/
+
 #ifndef COMMAND_SYNC_H
 #define COMMAND_SYNC_H
 
@@ -11,13 +41,20 @@ static const String COMMAND_SYNC_ADDRESS("ipc://sandbox/command_sync");
 static const String COMMAND_SYNC_ADDRESS("ipc:///tmp/command_sync");
 #endif
 
+static const String COMMAND_SYNC_MONITOR_ADDRESS("inproc://command_sync_monitor");
+
 class CommandSync : public Node {
 	GDCLASS(CommandSync, Node);
 
 	static CommandSync *singleton;
 
 	zmqpp::socket sock;
+
 	Callable execute_function;
+
+	// Monitoring
+	zmqpp::socket monitor_sock;
+	bool peer_disconnected = false;
 
 protected:
 	static void _bind_methods();
@@ -28,6 +65,10 @@ public:
 	void send_command(const Ref<Command> &p_command);
 	void send_command(const String &p_name);
 	void send_command(const String &p_name, const Array &p_args);
+
+	// Monitor peer connection and report status.
+	void poll_monitor();
+	bool is_peer_connected() const { return !peer_disconnected; }
 
 	Variant call_execute_function(const Ref<Command> &p_command);
 	void set_execute_function(Callable p_execute_function) { execute_function = p_execute_function; }
